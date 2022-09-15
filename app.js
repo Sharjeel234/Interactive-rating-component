54f96e6a5921d398100731fa61d173286042745c
Sharjeel Mansoor <sharjeelmansoor234@gmail.com>
2022-09-15 07:06:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const card = document.querySelector('.card')
 const thankCard = document.querySelector('.thank')
-const rating = document.querySelectorAll('.rate li')
+const rating = document.querySelectorAll('.rate .list')
 const rateNum = document.querySelector('.rateNum')
 const btn = document.querySelector('.card-btn')
 
 rating.forEach((rate) =>{
     rate.addEventListener('click', ()=> {
         rateNum.innerHTML = rate.innerHTML;
-        rate.classList.add('focus')
     })
 })
 
@@ -15,3 +14,15 @@ btn.addEventListener('click', ()=>{
     card.style.display = 'none';
     thankCard.style.display = 'flex'
 })
+
+for(let i =0; i<rating.length; i++)
+{
+    rating[i].onclick = function(){
+        let j = 0;
+        while(j < rating.length)
+        {
+            rating[j++].className = 'list'
+        }
+        rating[i].className = 'list focus'
+    }
+}
